9fed38a618f23b15fc956c6cca66f82ae5e305ab
Roeland Jago Douma <roeland@famdouma.nl>
2019-09-07 20:23:57 +0200

    Remove unneeded parameter
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- templates/personal.php
+++ templates/personal.php
@@ -2,6 +2,4 @@
 script('twofactor_nextcloud_notification', 'settings');
 ?>
 
-<input type="hidden" id="twofactor-notifications-initial-state" value="<?php p($_['enabled'] ? 'true' : 'false'); ?>">
-
 <div id="twofactor-notification-settings"></div>

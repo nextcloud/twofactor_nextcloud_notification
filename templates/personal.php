b48a2b8945299de6d2815ae6870630ff20578146
Joas Schilling <coding@schilljs.com>
2021-06-15 15:36:18 +0200

    Import the new script paths
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- templates/personal.php
+++ templates/personal.php
@@ -1,5 +1,5 @@
 <?php
-script('twofactor_nextcloud_notification', 'settings');
+script('twofactor_nextcloud_notification', 'twofactor_nextcloud_notification-settings');
 ?>
 
 <div id="twofactor-notification-settings"></div>

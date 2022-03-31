d8f16907fb67c4ea927038d9222b4fffb806a00f
Joas Schilling <coding@schilljs.com>
2022-03-31 17:09:51 +0200

    Move enable state to preferences
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ lib/Migration/Version3004Date20220331145316.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright Copyright (c) 2022 Joas Schilling <coding@schilljs.com>
+ *
+ * @author Joas Schilling <coding@schilljs.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\TwoFactorNextcloudNotification\Migration;
+
+use Closure;
+use OCP\IConfig;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+class Version3004Date20220331145316 extends SimpleMigrationStep {
+
+	/** @var IConfig */
+	protected $config;
+
+	public function __construct(IConfig $config) {
+		$this->config = $config;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options): void {
+		$keys = $this->config->getAppKeys('twofactor_nextcloud_notification');
+		foreach ($keys as $key) {
+			if (substr($key, -8) === '_enabled') {
+				$this->config->setUserValue(
+					substr($key, 0, -8),
+					'twofactor_nextcloud_notification',
+					'enabled',
+					'1'
+				);
+
+				$this->config->deleteAppValue('twofactor_nextcloud_notification', $key);
+			}
+		}
+	}
+}

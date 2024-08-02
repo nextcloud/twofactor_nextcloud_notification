03ab945306313c5e981482f573965551f998c8ca
Joas Schilling <coding@schilljs.com>
2024-08-02 15:55:35 +0200

    fix(db): Make database structure oracle compatible
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ lib/Migration/Version4000Date20240802134536.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\TwoFactorNextcloudNotification\Migration;
+
+use Closure;
+use OCA\TwoFactorNextcloudNotification\AppInfo\Application;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+class Version4000Date20240802134536 extends SimpleMigrationStep {
+	/**
+	 * @param IOutput $output
+	 * @param Closure(): ISchemaWrapper $schemaClosure
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		if ($schema->hasTable(Application::APP_ID . '_tokens')) {
+			$schema->dropTable(Application::APP_ID . '_tokens');
+			return $schema;
+		}
+		return null;
+	}
+}

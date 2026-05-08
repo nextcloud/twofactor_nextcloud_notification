9c869d73e52fad5e928bfbd9c03822c86fc162fb
Joas Schilling <coding@schilljs.com>
2026-05-08 09:21:50 +0200

    ci(rector): Add rector
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ rector.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+use Nextcloud\Rector\Set\NextcloudSets;
+use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+
+return RectorConfig::configure()
+	->withPaths([
+		__DIR__ . '/appinfo',
+		__DIR__ . '/lib',
+		__DIR__ . '/templates',
+		__DIR__ . '/tests',
+	])
+	->withPhpSets(php82: true)
+	->withSets([
+		PHPUnitSetList::PHPUNIT_110,
+		NextcloudSets::NEXTCLOUD_34,
+	])
+	->withTypeCoverageLevel(0);
